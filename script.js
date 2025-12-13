d333f3f39df9870592e39cb20e8b1e227cc811ec
yusariusly <yusariusly@gmail.com>
2025-12-13 13:08:30 +0700

    Read input value and display text

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 console.log("Halo JavaScript, aku mulai belajar");
+
+let judul = document.getElementById("judul");
+let tombol = document.getElementById("tombol");
+
+let namaInput = document.getElementById("namaInput");
+let tombolKirim = document.getElementById("kirim");
+
+tombol.addEventListener("click", function () {
+  judul.innerText = "Tombol atas diklik";
+});
+
+tombolKirim.addEventListener("click", function () {
+  let nama = namaInput.value;
+  judul.innerText = "Halo " + nama;
+});
