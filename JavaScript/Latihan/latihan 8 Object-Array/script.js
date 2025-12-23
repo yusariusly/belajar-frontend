ae83ef1f9e0e2782ef2ee42ae9774bc0e651ba3d
yusariusly <yusariusly@gmail.com>
2025-12-23 16:39:12 +0700

    Latihan Object dan Array

---
+++ JavaScript/Latihan/latihan 8 Object-Array/script.js
@@ -0,0 +1,37 @@
+//latihan 1
+let motor = {
+  merk: "Honda",
+  tipe: "Beat",
+  tahun: 2022
+};
+
+console.log(motor.tipe);
+
+//latihan 2
+let angka = [5, 10, 15, 20];
+console.log(angka[3]);
+
+//latihan 3
+let produk = [
+  { barang: "laptop", harga:5000000 },
+  { barang: "HP", harga: 3000000 }
+];
+console.log(produk[1].harga);
+
+//latihan 4
+
+//latihan 5
+let hasil = document.getElementById ("hasil");
+let inputData = document.getElementById ("inputData");
+let simpan = document.getElementById ("simpan")
+
+simpan.addEventListener ("click", function () {
+  let input = inputData.value;
+
+  if (input === "") {
+    hasil.innerText = "Input tidak boleh kosong";
+  }else {
+    hasil.innerText = "Data tersimpan";
+  }
+  
+});
